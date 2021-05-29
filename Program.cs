f8899d9b34f494ca9dd475cfb02d51d6088dbcfb
orlando <75084509+dehoisted@users.noreply.github.com>
2021-05-29 18:51:27 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-//Example
+// Example
 
 using System;
 using System.Threading;
@@ -8,9 +8,12 @@ namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int w_seconds = 10;
+        public static string[] messages = { "@everyone skid", "kyle is black", "joshy is a fluffy femboy", "depressed/broken :/" };
+
+        static void Main()
         {
-            TelegramBot TBot = new TelegramBot("BOTID", "CHATID");
+            TelegramBot TBot = new TelegramBot("1850958171:AAGEygAAsxl_r5WkDBn-Qp5Wti6pMGSChbY", "-1001333610567");
             switch (TBot.IsRunning())
             {
                 case true:
@@ -22,25 +25,38 @@ namespace ConsoleApp
                     break;
             }
 
-            TBot.SendMessage("Bot is on!");
-            Thread.Sleep(1000);
+            TBot.SendMessage("Bot is online!");
+            Thread.Sleep(2000);
 
             while (true)
             {
-                switch (TBot.GetRand(1, 3))
+                switch (TelegramBot.GetRand(0, 3))
                 {
-                    case 1:
-                        TBot.SendMessage("skid");
+                    case 0:
+                        TBot.SendMessage(messages[0]);
+                        Console.WriteLine("Sent message [" + messages[0] + "]");
                         break;
+
+                    case 1:
+                        TBot.SendMessage(messages[1]);
+                        Console.WriteLine("Sent message [" + messages[1] + "]");
+                        break;
+
                     case 2:
-                        TBot.SendMessage("joshy is a fluffy femboy");
+                        TBot.SendMessage(messages[2]);
+                        Console.WriteLine("Sent message [" + messages[2] + "]");
                         break;
 
                     case 3:
-                        TBot.SendMessage("kyle is black");
+                        TBot.SendMessage(messages[3]);
+                        Console.WriteLine("Sent message [" + messages[3] + "]");
                         break;
                 }
-                Thread.Sleep(10000);
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Sleeping for " + w_seconds + " seconds");
+                Console.ResetColor();
+                Thread.Sleep(w_seconds * 1000);
             }
         }
     }
